81b10c18ee8f61e50854e4797ee225811e925dcd
Mian Mohid Naeem <113250876+nexcodes@users.noreply.github.com>
2026-02-26 17:29:37 +0500

    feat: Implement TreeView component and integrate it into FileExplorer for improved file navigation

--- src/components/file-explorer.tsx
+++ src/components/file-explorer.tsx
@@ -18,8 +18,9 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import { getLanguageFromExtension } from "@/lib/utils";
+import { convertFilesToTreeItems, getLanguageFromExtension } from "@/lib/utils";
 import CodeView from "./code-view";
+import TreeView from "./tree-view";
 
 type FileCollection = { [path: string]: string };
 
@@ -28,31 +29,58 @@ interface FileExplorerProps {
 }
 
 const FileExplorer = ({ files }: FileExplorerProps) => {
+  const [copied, setCopied] = useState(false);
   const [selectedFile, setSelectedFile] = useState<string | null>(() => {
     const fileKeys = Object.keys(files);
     return fileKeys.length > 0 ? fileKeys[0] : null;
   });
 
+  const treeData = useMemo(() => {
+    return convertFilesToTreeItems(files);
+  }, [files]);
+
+  const handleFileSelect = useCallback(
+    (filePath: string) => {
+      if (files[filePath]) {
+        setSelectedFile(filePath);
+      }
+    },
+    [files],
+  );
+
+  const handleCopy = () => {
+    if (selectedFile) {
+      navigator.clipboard.writeText(files[selectedFile]);
+      setCopied(true);
+
+      setTimeout(() => setCopied(false), 2000);
+    }
+  };
+
   return (
     <ResizablePanelGroup direction="horizontal">
       <ResizablePanel defaultSize={30} minSize={30} className="bg-sidebar">
-        <p>TODO: TREEVIEW</p>
+        <TreeView
+          data={treeData}
+          value={selectedFile}
+          onSelect={handleFileSelect}
+        />
       </ResizablePanel>
       <ResizableHandle className="hover:bg-primary transition-colors" />
       <ResizablePanel defaultSize={70} minSize={50} className="bg-sidebar">
         {selectedFile && files[selectedFile] ? (
           <div className="h-full w-full flex flex-col">
             <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
-              {/* TODO: File breadcrumb */}
+              <FileBreadCrumb filePath={selectedFile} />
               <Hint text="Copy to clipboard" side="bottom">
                 <Button
                   variant="outline"
                   size="icon"
                   className="ml-auto"
-                  onClick={() => {}}
-                  disabled={false}
+                  onClick={handleCopy}
+                  disabled={copied}
                 >
-                  <CopyIcon />
+                  {copied ? <CopyCheckIcon /> : <CopyIcon />}
                 </Button>
               </Hint>
             </div>
@@ -74,3 +102,62 @@ const FileExplorer = ({ files }: FileExplorerProps) => {
 };
 
 export default FileExplorer;
+
+interface FileBreadcrumbProps {
+  filePath: string;
+}
+
+const FileBreadCrumb = ({ filePath }: FileBreadcrumbProps) => {
+  const pathSegments = filePath.split("/");
+  const maxSegments = 3;
+
+  const renderBreadCrumbItems = () => {
+    if (pathSegments.length <= maxSegments) {
+      return pathSegments.map((segment, index) => {
+        const isLast = index === pathSegments.length - 1;
+
+        return (
+          <Fragment key={index}>
+            <BreadcrumbItem>
+              {isLast ? (
+                <BreadcrumbPage className="font-medium">
+                  {segment}
+                </BreadcrumbPage>
+              ) : (
+                <span className="text-muted-foreground">{segment}</span>
+              )}
+            </BreadcrumbItem>
+            {!isLast && <BreadcrumbSeparator />}
+          </Fragment>
+        );
+      });
+    } else {
+      const firstSegment = pathSegments[0];
+      const lastSegment = pathSegments[pathSegments.length - 1];
+
+      return (
+        <Fragment>
+          <BreadcrumbItem>
+            <span className="text-muted-foreground">{firstSegment}</span>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbEllipsis />
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="font-medium">
+                {lastSegment}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbItem>
+        </Fragment>
+      );
+    }
+  };
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>{renderBreadCrumbItems()}</BreadcrumbList>
+    </Breadcrumb>
+  );
+};
